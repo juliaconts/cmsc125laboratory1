e91d9c5acbd51a52ccd6068077c98f3076f8ce00
juliaconts <jmcontreras3@up.edu.ph>
2026-02-05 00:03:39 +0800

    docs: added weekly todo comments

--- executor.c
+++ executor.c
@@ -5,32 +5,63 @@
 #include <string.h>
 #include "mysh.h"
 
-void execute_command(Command *cmd) {
-    if (cmd->command == NULL) return;
+void execute_command(Command *cmd)
+{
+    if (cmd->command == NULL)
+        return;
 
     // Built-ins
-    if (strcmp(cmd->command, "exit") == 0) exit(0);
-    if (strcmp(cmd->command, "cd") == 0) {
-        if (cmd->args[1]) chdir(cmd->args[1]);
+    if (strcmp(cmd->command, "exit") == 0)
+        exit(0);
+    if (strcmp(cmd->command, "cd") == 0)
+    {
+        if (cmd->args[1])
+            chdir(cmd->args[1]);
         return;
     }
+
+    // TODO:WEEK3 - setting the & logic
+    /* insert code here*/
 
     // External Command
     pid_t pid = fork();
 
-    if (pid == 0) {
+    if (pid == 0)
+    {
         // CHILD PROCESS
+        // TODO:WEEK3 - redirection for open, dup2 must happen here
+        /* insert code here */
+
+        // TODO:WEEK2 - file redirection handling should be done here
+        /* insert code here */
         execvp(cmd->command, cmd->args);
-        perror("mysh"); 
+        perror("mysh");
         exit(1);
-    } else if (pid > 0) {
+    }
+    else if (pid > 0)
+    {
         // PARENT PROCESS
-        if (!cmd->background) {
+
+        // TODO:WEEK3 - checks if backgroound job (&)
+        /* insert code here */
+
+        // TODO:WEEK2 - no file redirection needed here (parent waits for the process)
+        /* insert code here*/
+        if (!cmd->background)
+        {
             waitpid(pid, NULL, 0);
-        } else {
+        }
+        else
+        {
+            // TODO:WEEK3 - put foreground job where parent does not wait
             printf("[%d] Started PID: %d\n", 1, pid);
         }
-    } else {
+
+        // TODO:WEEK3 - zombie reaping code goes here
+    }
+    else
+    {
+        // fork failure handling
         perror("fork failed");
     }
 }
--- parser.c
+++ parser.c
@@ -2,20 +2,46 @@
 #include <stdlib.h>
 #include "mysh.h"
 
-void parse_command(char *input, Command *cmd) {
+void parse_command(char *input, Command *cmd)
+{
+
+    // TODO:Week3 - an edge case handling for checking if the input is null or empty string will go here
+    /* insert code here*/
+
     int i = 0;
+
+    // TODO:Week3 - edge case handling for whitespaces will go here
+    /* insert code here*/
+
     char *token = strtok(input, " \t\n");
 
-    while (token != NULL && i < 255) {
-        // Week 1: Detect background flag
-        if (strcmp(token, "&") == 0) {
+    // TODO:Week3 - memory safety to limit no of args to avoid overflow
+    /* insert code here*/
+
+    while (token != NULL && i < 255)
+    {
+
+        // TODO:Week3 - edge case handling for validation of empty strings
+        /* insert code here*/
+
+        // Week1 - Detect background flag
+        if (strcmp(token, "&") == 0)
+        {
             cmd->background = true;
         }
-        // Week 2: Redirection parsing will go here
-        else {
-            if (i == 0) cmd->command = token;
+        // TODO:Week2 - Redirection parsing will go here
+        else
+        {
+
+            // TODO:Week3 - memory safety
+            /* insert code here*/
+
+            if (i == 0)
+                cmd->command = token;
             cmd->args[i++] = token;
         }
+
+        // TODO:Week3 - edge case handling to ensure strtok not return unwanted null
         token = strtok(NULL, " \t\n");
     }
     cmd->args[i] = NULL; // EXECVP needs a NULL terminator
